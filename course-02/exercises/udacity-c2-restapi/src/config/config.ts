b58b559efc0c89114bf81f40eb5ceb762418ab08
garcialuis <garcialuisdev@gmail.com>
2020-07-11 23:57:51 -0700

    [3-L4] Completed (3. Fullstack) Lesson 4: *Modified config file to use env vars to connect to storage services, +completed functions in feed.router.ts to fetch and update data records using sequelize

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.UD_DB_USER,
+    "password": process.env.UD_DB_PASSWORD,
+    "database": process.env.UD_DB_DB,
+    "host": process.env.UD_DB_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": process.env.UD_AWS_REGION,
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": process.env.UD_AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
